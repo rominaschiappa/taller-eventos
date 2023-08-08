8cd4e117c46e6aca41e00fd3bce2ad24b5d43053
rominaschiappa <romischiappa123@gmail.com>
2023-08-07 21:36:48 -0300

    html

--- eventos.js
+++ eventos.js
@@ -1,13 +1,3 @@
-const saludarBtn = document.getElementById("saludarBtn");
-const divContainer = document.getElementById("divContainer");
-const divBtn = document.getElementById("divBtn");
-
-// Manejador de evento para el botón de saludar
-saludarBtn.addEventListener("click", function () {
-    alert("Hola!");
-});
-
-// Manejador de evento para el botón en el div
-divBtn.addEventListener("click", function () {
+document.getElementById(saludarButton).addEventListener(click, function () {
     alert("Hola! Soy el div");
-});
+  });
