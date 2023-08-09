3f2e99f58b8e5d982477bd9da23aca18f2e21a08
rominaschiappa <romischiappa123@gmail.com>
2023-08-08 21:00:05 -0300

    Ejercicio semana 7

--- eventos.js
+++ eventos.js
@@ -1,3 +1,7 @@
-document.getElementById(saludarButton).addEventListener(click, function () {
+// Obtener referencia al div
+const divConBoton = document.querySelector('.boton-con-fondo');
+
+// Agregar el manejador de eventos al hacer clic en el div
+divConBoton.addEventListener('click', function() {
     alert("Hola! Soy el div");
-  });
+});
